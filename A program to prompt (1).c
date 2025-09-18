ef082fea725121a421e93c64928cfda599da10b9
Taiku662 <njeritaiku@gmail.com>
2025-09-18 09:33:02 +0300

    Add files via upload

---
+++ A program to prompt (1).c
@@ -0,0 +1,29 @@
+//A program to prompt
+/*
+Name: Taiku Purity
+Reg No:CT100/G/26152/25
+Description:A PROMPT
+*/
+
+//main function 
+# include<stdio.h>
+int main ()
+ {
+    int height ;
+    int contact;
+    int weight ;
+    
+    printf("enter your height");
+    scanf("%d,&height");
+    printf("your height is%d \n,height");
+
+    printf("enter your phone number");
+    scanf("%d,&phone number");
+    printf("your phone number is %d \n,contact");
+
+    printf("enter your weight");
+    scanf("%d,&weight");
+    printf("your weight is %d \n,weight");
+
+    return 0;
+}
---
+++ semesterunits.c
@@ -0,0 +1,22 @@
+//SEMESTER UNITS 
+/*
+Name: Taiku Purity
+Reg No:CT100/G/26152/25
+Description:SEM 1 UNITS
+*/
+
+//main function 
+# include<stdio.h>
+int main ()
+ {
+    printf("Analogue electronics \n"); 
+    printf("Computer systems and organisation \n");
+    printf("Mathematics for science \n");
+    printf("introduction to proramming \n");
+    printf("essentials of accounting \n");
+    printf("Communication skills\n");
+    printf("Fundamentals of ICT\n");
+    printf("introduction to computer applications\n");
+    
+    return 0;
+}
