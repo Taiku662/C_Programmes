b8d5754b257e6eefd6d2183814399027467e60ac
Taiku662 <njeritaiku@gmail.com>
2025-10-03 00:03:42 +0300

    Add files via upload

---
+++ NewFile.c
@@ -0,0 +1,46 @@
+/*
+*/
+#include <stdio.h>
+int main()
+{
+int choice;
+printf("Mobile data offers: \n");
+printf("|option|bundles|cost(KES)| \n");
+printf("|....|....|....|");
+printf("|1|100MB|50| \n");
+printf("|2|500MBS|200| \n");
+printf("|3|1GB|350| \n");
+printf("|4|2GB|600|");
+
+printf("enter your choice from 1-4 :\n");
+scanf("%d",choice);
+
+switch(choice)
+{
+case 1:
+printf("Your selection:100MB \n");
+printf("cost:50KES \n");
+	break;
+
+case 2:
+	printf("your selection:500MBS \n");
+	printf("cost:200KES \n");
+	break;
+
+case 3:
+	printf("your selection:1GB |n");
+	printf("cost:350KES \n");
+	break;
+
+case 4:
+	printf("your selection:2GB \n");
+	printf("cost:600KES \n");
+	break;
+
+default:
+	printf("Invalid choice!select choice 1-4. \n");
+	break;
+}
+	
+	return 0;
+}
