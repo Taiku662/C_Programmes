41207327cf7fc2dd33f2ad8d709b7be5124d1534
Taiku662 <njeritaiku@gmail.com>
2025-10-09 11:29:13 +0300

    Add files via upload

---
+++ numbersfrom100-50.c
@@ -0,0 +1,18 @@
+/*
+PURITY TAIKU
+CT100/G/26152/25
+for loop printing numbers from 100-50
+*/
+
+#include <stdio.h>
+
+int main()
+{
+int i;	
+//for loop printing numbers from 100-50
+for(i=100; i>=50; i--){
+	printf("%d \n",i);
+}
+
+	return 0;
+}
