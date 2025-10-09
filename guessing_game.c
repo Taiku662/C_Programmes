c055dcaed247e0fa7229627427393c60757ef8c4
Taiku662 <njeritaiku@gmail.com>
2025-10-09 19:34:40 +0300

    Update guessing_game.c

--- guessing_game.c
+++ guessing_game.c
@@ -1,10 +1,7 @@
-/* 2. Write a C program that implements a number guessing game. The computer should generate a
-random number in the range 1 to 20 (inclusive). The program should repeatedly prompt the
-player to enter a guess and then respond with one of the following messages:
-• "Too high!" if the guess is greater than the secret number.
-• "Too low!" if the guess is less than the secret number.
-• "Congratulations!" if the guess is equal to the secret number.The program must also count and display the total number of attempts it took for the player to
-guess correctly.
+/* 
+PURITY TAIKU
+CT100/G/26152/25
+DESCRIPTION:A while loop program on number guessing game
 */
 
 #include <stdio.h>
@@ -44,4 +41,5 @@ printf("it took you %d tries",tries);
 }
 	
 	return 0;
+
 }
